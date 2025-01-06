0718c20eed3389303c8f35b9e6cdb0064e055e62
AcheampongAndy <acheampongandrews1999@gmail.com>
2025-01-06 11:34:32 +0000

    init

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -26,7 +26,7 @@ def create_app(test_config=None):
     # Home Page
     @app.route('/')
     def base():
-        return render_template('dashboard.html')
+        return render_template('base.html')
     
     from . import db
     db.init_app(app)
